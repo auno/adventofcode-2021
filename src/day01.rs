6b6fd6071ac586297b4dfd7951593c4ac33ec426
Mikael Auno <mikael@auno.se>
2021-12-01 10:11:47 +0100

    Finished 2021/01b

--- src/day01.rs
+++ src/day01.rs
@@ -17,6 +17,19 @@ fn part1(nums: &[i32]) -> usize {
         .count()
 }
 
+#[aoc(day1, part2)]
+fn part2(nums: &[i32]) -> usize {
+    let sums: Vec<i32> = nums
+        .windows(3)
+        .map(|w| w[0] + w[1] + w[2])
+        .collect();
+
+    sums
+        .windows(2)
+        .filter(|w| w[1] > w[0])
+        .count()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -24,5 +37,10 @@ mod tests {
     #[test]
     fn part1_example() {
         assert_eq!(7, part1(&parse(include_str!("../input/2021/day1.part1.test.7.txt")).unwrap()));
+    }
+
+    #[test]
+    fn part2_example() {
+        assert_eq!(5, part2(&parse(include_str!("../input/2021/day1.part2.test.5.txt")).unwrap()));
     }
 }
