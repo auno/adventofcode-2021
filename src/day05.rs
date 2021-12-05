14e02cf241a2c7205e5988dc6fa49e1878cea1fb
Mikael Auno <mikael@auno.se>
2021-12-05 14:07:07 +0100

    Finished 2021/5b

--- src/day05.rs
+++ src/day05.rs
@@ -27,19 +27,31 @@ fn parse(input: &str) -> Result<Vec<(Point, Point)>, ParseIntError> {
     )
 }
 
+fn range(a: i32, b: i32) -> Vec<i32> {
+    if b >= a {
+        (a..=b).collect::<Vec<i32>>()
+    } else {
+        (b..=a).rev().collect::<Vec<i32>>()
+    }
+}
+
 fn mark(points: &mut HashMap<Point, usize>, a: &Point, b: &Point) {
     match (a, b) {
-        (c, d) if (c.x == d.x && c.y <= d.y) => {
-            for y in c.y..=d.y {
+        (c, d) if (c.x == d.x) => {
+            for y in range(c.y, d.y) {
                 *points.entry(Point { x: c.x, y }).or_default() += 1;
             }
         },
-        (c, d) if (c.y == d.y && c.x <= d.x) => {
-            for x in c.x..=d.x {
+        (c, d) if (c.y == d.y) => {
+            for x in range(c.x, d.x) {
                 *points.entry(Point { x, y: c.y }).or_default() += 1;
             }
         },
-        (c, d) => mark(points, d, c)
+        (c, d) => {
+            for (x, y) in range(c.x, d.x).into_iter().zip(range(c.y, d.y)) {
+                *points.entry(Point { x, y }).or_default() += 1;
+            }
+        }
     }
 }
 
@@ -61,6 +73,20 @@ fn part1(lines: &[(Point, Point)]) -> usize {
         .count()
 }
 
+#[aoc(day5, part2)]
+fn part2(lines: &[(Point, Point)]) -> usize {
+    let mut points: HashMap<Point, usize> = HashMap::new();
+
+    for (a, b) in lines {
+        mark(&mut points, a, b);
+    }
+
+    points
+        .iter()
+        .filter(|(_, c)| **c > 1)
+        .count()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -68,5 +94,10 @@ mod tests {
     #[test]
     fn part1_example() {
         assert_eq!(5, part1(&parse(include_str!("../input/2021/day5.part1.test.5.txt")).unwrap()));
+    }
+
+    #[test]
+    fn part2_example() {
+        assert_eq!(12, part2(&parse(include_str!("../input/2021/day5.part2.test.12.txt")).unwrap()));
     }
 }
