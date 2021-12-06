44f8fe15b39b1764bcf5bcd4a6f08a7911761852
Mikael Auno <mikael@auno.se>
2021-12-06 11:58:13 +0100

    Finished 2021/6b

--- src/day06.rs
+++ src/day06.rs
@@ -12,24 +12,35 @@ fn parse(input: &str) -> Result<Vec<i32>, ParseIntError> {
     )
 }
 
-#[aoc(day6, part1)]
-fn part1(nums: &[i32]) -> usize {
-    let mut nums: Vec<i32> = nums.iter().copied().collect();
-    let mut next_nums: Vec<i32> = vec![];
+fn simulate(nums: &[i32], iterations: usize) -> usize {
+    let mut counts: [usize; 9] = [0; 9];
 
-    for _iteration in 0..80 {
-        for num in &nums {
-            match num {
-                0 => { next_nums.push(6); next_nums.push(8); }
-                v => { next_nums.push(v - 1); }
-            }
-        }
-
-        nums = next_nums;
-        next_nums = vec![];
+    for num in nums {
+        counts[*num as usize] += 1
     }
 
-    nums.len()
+    for _iteration in 0..iterations {
+        let zero = counts[0];
+
+        for i in 0..8 {
+            counts[i] = counts[i + 1];
+        }
+
+        counts[6] += zero;
+        counts[8] = zero;
+    }
+
+    counts.iter().sum()
+}
+
+#[aoc(day6, part1)]
+fn part1(nums: &[i32]) -> usize {
+    simulate(nums, 80)
+}
+
+#[aoc(day6, part2)]
+fn part2(nums: &[i32]) -> usize {
+    simulate(nums, 256)
 }
 
 #[cfg(test)]
@@ -39,5 +50,10 @@ mod tests {
     #[test]
     fn part1_example() {
         assert_eq!(5934, part1(&parse(include_str!("../input/2021/day6.part1.test.5934.txt")).unwrap()));
+    }
+
+    #[test]
+    fn part2_example() {
+        assert_eq!(26984457539, part2(&parse(include_str!("../input/2021/day6.part2.test.26984457539.txt")).unwrap()));
     }
 }
