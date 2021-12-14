5f532dddd5295112117dfb53ebd00efa4f0f19a6
Mikael Auno <mikael@auno.se>
2021-12-14 16:22:29 +0100

    Finished 2021/14b

--- src/day14.rs
+++ src/day14.rs
@@ -46,6 +46,50 @@ fn part1((template, rules): &(Vec<char>, HashMap<(char, char), char>)) -> usize 
     max - min
 }
 
+#[aoc(day14, part2)]
+fn part2((template, rules): &(Vec<char>, HashMap<(char, char), char>)) -> usize {
+    let mut pairs = template
+        .windows(2)
+        .map(|w| (w[0], w[1]))
+        .counts();
+
+    for _step in 0..40 {
+        let mut next_pairs = pairs.clone();
+
+        for ((a, b), count) in pairs.into_iter() {
+            if count == 0 {
+                continue;
+            }
+
+            if let Some(rule) = rules.get(&(a, b)) {
+                *next_pairs.entry((a, b)).or_insert(0) -= count;
+                *next_pairs.entry((a, *rule)).or_insert(0) += count;
+                *next_pairs.entry((*rule, b)).or_insert(0) += count;
+            }
+        }
+
+        pairs = next_pairs;
+    }
+
+    let mut individual_counts: HashMap<char, usize> = pairs
+        .iter()
+        .flat_map(|((a, b), count)| [(a, count), (b, count)])
+        .fold(HashMap::new(), |mut acc, (c, count)| {
+            *acc.entry(*c).or_insert(0) += count;
+            acc
+        });
+
+    *individual_counts.entry(*template.first().unwrap()).or_default() += 1;
+    *individual_counts.entry(*template.last().unwrap()).or_default() += 1;
+
+    for (_, count) in individual_counts.iter_mut() {
+        *count /= 2;
+    }
+
+    let ((_, min), (_, max)) = individual_counts.iter().minmax_by_key(|(_, count)| **count).into_option().unwrap();
+
+    max - min
+}
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -53,5 +97,10 @@ mod tests {
     #[test]
     fn part1_example() {
         assert_eq!(1588, part1(&parse(include_str!("../input/2021/day14.part1.test.1588.txt"))));
+    }
+
+    #[test]
+    fn part2_example() {
+        assert_eq!(2188189693529, part2(&parse(include_str!("../input/2021/day14.part2.test.2188189693529.txt"))));
     }
 }
